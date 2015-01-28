5ae9752273ec57d3010c87d9769dec1d9868a8d9
Kris Kumler <kris@triceimaging.com>
2015-01-28 15:14:03 -0500

    Applying suggestions from audit --strict

--- dcmtk361.rb
+++ dcmtk361.rb
@@ -1,23 +1,22 @@
-require 'formula'
-
 # Formula copied from dcmtk formula
 class Dcmtk361 < Formula
-  homepage 'http://dicom.offis.de/dcmtk.php.en'
-  url 'http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20140617.tar.gz'
+  homepage "http://dicom.offis.de/dcmtk.php.en"
+  url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20140617.tar.gz"
   version "3.6.1-20140617"
-  sha256 'c0aab5a3809e20f8b4eae2f181c77a1668d0665e86a590821a59808f45656cdc'
+  sha256 "c0aab5a3809e20f8b4eae2f181c77a1668d0665e86a590821a59808f45656cdc"
 
   bottle do
-    sha1 "8c89405648fc176e34716a6e786444da20765b12" => :yosemite
+    sha1 "ae36fa23b94d89b8e3d4a986b6f205bc287ce0e1" => :yosemite
   end
 
-  option 'with-docs', 'Install development libraries/headers and HTML docs'
-  option 'with-openssl', 'Configure DCMTK with support for OpenSSL'
+  option "with-docs", "Install development libraries/headers and HTML docs"
+  option "with-openssl", "Configure DCMTK with support for OpenSSL"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
   depends_on "libpng"
-  depends_on 'libtiff'
+  depends_on "libtiff"
   depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "openssl" => :optional
 
   conflicts_with "dcmtk", :because => "Differing versions of same package"
 
@@ -25,14 +24,14 @@ class Dcmtk361 < Formula
     ENV.m64 if MacOS.prefer_64_bit?
 
     args = std_cmake_args
-    args << '-DDCMTK_WITH_DOXYGEN=YES' if build.with? "docs"
-    args << '-DDCMTK_WITH_OPENSSL=YES' if build.with? "openssl"
-    args << '..'
+    args << "-DDCMTK_WITH_DOXYGEN=YES" if build.with? "docs"
+    args << "-DDCMTK_WITH_OPENSSL=YES" if build.with? "openssl"
+    args << ".."
 
-    mkdir 'build' do
-      system 'cmake', *args
-      system 'make DOXYGEN' if build.with? "docs"
-      system 'make install'
+    mkdir "build" do
+      system "cmake", *args
+      system "make", "DOXYGEN" if build.with? "docs"
+      system "make", "install"
     end
   end
 end
